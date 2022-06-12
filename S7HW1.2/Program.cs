3027e6e4eab68d35be5f16e5f5639888d2488fbc
Lnosonova <lilipadnose@gmail.com>
2022-06-12 09:50:15 +0300

    Seminar7HW

---
+++ S7HW1.2/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 1. В двумерном массиве целых чисел.
+//Удалить строку и столбец, на пересечении которых расположен наименьший элемент.
+
+void FillMultiArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void PrintMultiArray(int[,] matr)// Метод вывода многомерного массива
+{
+    for (int i = 0; i < matr.GetLength(0); i++) //matrix.GetLength(0) - количество строк
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)// matrix.GetLength(1) - количество столбцов
+        {
+            System.Console.Write($"{matr[i, j]} ");
+        }
+        System.Console.WriteLine();// Для вывода массива в виде таблицы 3*4
+    }
+}
+
+int MinNumberMatrix(int[,] matrix) // Метод нахождения минимального элемента матрицы
+{
+    int Min = matrix[1, 1];
+    for (int k = 0; k < matrix.GetLength(0); k++)
+    {
+        for (int l = 0; l < matrix.GetLength(1); l++)
+        {
+            
+            if (matrix[k, l] < Min) Min = matrix[k, l];
+
+        }
+
+    }
+    return Min;
+}
+
+int[,] RealMatrix = new int[3, 4];
+FillMultiArray(RealMatrix);
+PrintMultiArray(RealMatrix);
+int Minimum = MinNumberMatrix(RealMatrix);
+System.Console.WriteLine(Minimum);
---
+++ S7HW3/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 3. Сформировать трехмерный массив не повторяющимися двузначными числами
+// показать его построчно на экран выводя индексы соответствующего элемента
+
+void FillMultiArray(int[,,] matr)
+{
+    int count = 10;
+    int xar = 3;
+    int yar = 5;
+    int zar = 6;
+
+    for (int x = 0; x < xar; x++)
+    {
+        for (int y = 0; y < yar; y++)
+        {
+            for (int z = 0; z < zar; z++)
+            {
+                matr[x, y, z] = count;
+                count = count + 1;
+                System.Console.WriteLine($"Элемент ({x},{y},{z}) = {matr[x, y, z]}");
+            }
+        }
+
+    }
+}
+
+
+void PrintMultiArray(int[,,] matr)
+{
+    int xar = 3;
+    int yar = 5;
+    int zar = 6;
+
+    for (int z = 0; z < zar; z++)
+    {
+        for (int x = 0; x < xar; x++)
+        {
+            for (int y = 0; y < yar; y++)
+            {
+                System.Console.WriteLine($"Элемент ({x},{y},{z}) = {matr[x, y, z]}");
+            }
+        }
+
+    }
+}
+
+
+
+int[,,] RealMatrix = new int[3, 5, 6];
+FillMultiArray(RealMatrix);
+PrintMultiArray(RealMatrix);
