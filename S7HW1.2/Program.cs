25f32cff4397ea207e95c9a8a1d498597c78d564
Lnosonova <lilipadnose@gmail.com>
2022-06-22 00:05:11 +0300

    Add S7_HW1.2

--- S7HW1.2/Program.cs
+++ S7HW1.2/Program.cs
@@ -26,22 +26,116 @@ void PrintMultiArray(int[,] matr)// Метод вывода многомерно
 
 int MinNumberMatrix(int[,] matrix) // Метод нахождения минимального элемента матрицы
 {
-    int Min = matrix[1, 1];
+    int Min = matrix[0, 0];
     for (int k = 0; k < matrix.GetLength(0); k++)
     {
         for (int l = 0; l < matrix.GetLength(1); l++)
         {
-            
-            if (matrix[k, l] < Min) Min = matrix[k, l];
 
+            if (matrix[k, l] <= Min)
+            {
+                Min = matrix[k, l];
+            }
         }
-
     }
     return Min;
 }
 
-int[,] RealMatrix = new int[3, 4];
+void MinPosMatrix(int[,] matrix, int[] MinR, int[] MinC, int Min) // Метод нахождения позиции минимального элемента матрицы
+{
+    int i = 0;
+    int j = 0;
+    for (int k = 0; k < matrix.GetLength(0); k++)
+    {
+        for (int l = 0; l < matrix.GetLength(1); l++)
+        {
+
+            if (matrix[k, l] == Min)
+            {
+                if (Array.IndexOf(MinR, k + 1) == -1)
+                {
+                    MinR[i] = k + 1;
+                    i++;
+                }
+                if (Array.IndexOf(MinC, l + 1) == -1)
+                {
+                    MinC[j] = l + 1;
+                    j++;
+                }
+            }
+        }
+    }
+
+}
+
+
+
+void PrintNewArray(int[] Arr2, int count)
+{
+    for (int position = 0; position < count; position++)
+    {
+        System.Console.Write(Arr2[position] + " ");
+    }
+}
+
+
+
+int Row = 7;
+int Col = 9;
+int[,] RealMatrix = new int[Row, Col];
 FillMultiArray(RealMatrix);
 PrintMultiArray(RealMatrix);
+
+int nr = 0;//количество строк к удалению
+int nc = 0;//количество столбцов к удалению
+int[] MinRow = new int[Row * Col];
+int[] MinCol = new int[Row * Col];
+// for (int i = 0; i < Row * Col; i++)
+// {
+//     MinRow[i] = 0;
+//     MinCol[i] = 0;
+// }
+
 int Minimum = MinNumberMatrix(RealMatrix);
-System.Console.WriteLine(Minimum);
+MinPosMatrix(RealMatrix, MinRow, MinCol, Minimum);
+System.Console.WriteLine($"Минимальный элемент = {Minimum}");
+System.Console.WriteLine();
+System.Console.WriteLine("Строка положения минимального элемента:");
+nr = Array.IndexOf(MinRow, 0);
+nc = Array.IndexOf(MinCol, 0);
+PrintNewArray(MinRow, nr);
+System.Console.WriteLine();
+System.Console.WriteLine("Столбец положения минимального элемента:");
+PrintNewArray(MinCol, nc);
+System.Console.WriteLine();
+
+
+System.Console.WriteLine($"Количество удаляемых строк: {nr}");
+System.Console.WriteLine($"Количество удаляемых столбцов: {nc}");
+
+
+int[,] ResultArray = new int[Row - nr, Col - nc];
+int x = 0;
+int y = 0;
+for (int i = 0; i < Row; i++)
+{
+    if (Array.IndexOf(MinRow, i + 1) == -1)
+    {
+        for (int j = 0; j < Col; j++)
+        {
+            if (Array.IndexOf(MinCol, j + 1) == -1)
+            {
+                // System.Console.Write(RealMatrix[i, j]);
+                ResultArray[x, y] = RealMatrix[i, j];
+                y++;
+            }
+        }
+        // System.Console.WriteLine();
+        y = 0;
+        x++;
+    }
+}
+System.Console.WriteLine("Итоговый массив:");
+PrintMultiArray(ResultArray);
+
+
